1e854cc38cca2836888f43fbea791529b6ecb035
Jarlisonn <jarl.arrow@gmail.com>
2021-08-10 21:11:23 -0300

    Criação dos atributos, construtores, gets e sets dos models Embarcação, Empresa e Viagem

--- src/model/embarcacao/Embarcacao.java
+++ src/model/embarcacao/Embarcacao.java
@@ -1,7 +1,55 @@
 package model.embarcacao;
 
+import model.empresa.Empresa;
+
 public class Embarcacao {
-    public Embarcacao(){
-        
-    }
+	
+	private int id;
+	private String nome;
+	private int lotacao;
+	private Empresa propritário;
+	
+	//Construtor
+	public Embarcacao(int id, String nome, int lotacao, Empresa propritário) {
+		this.id = id;
+		this.nome = nome;
+		this.lotacao = lotacao;
+		this.propritário = propritário;
+	}
+    
+	//Métodos gets e sets
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getLotacao() {
+		return lotacao;
+	}
+
+	public void setLotacao(int lotacao) {
+		this.lotacao = lotacao;
+	}
+
+	public Empresa getPropritário() {
+		return propritário;
+	}
+
+	public void setPropritário(Empresa propritário) {
+		this.propritário = propritário;
+	}
+	
+	
+    
 }
--- src/model/viagem/Viagem.java
+++ src/model/viagem/Viagem.java
@@ -1,5 +1,49 @@
 package model.viagem;
 
+import java.util.*;
+import model.linha.Linha;
+
 public class Viagem {
+	
+	private int id;
+	private Date data;
+	private Linha linha;
+	
+	//Construtor
+	public Viagem(int id, Date data, Linha linha) {
+		this.id = id;
+		this.data = data;
+		this.linha = linha;
+	}
+    
+	//Métodos gets e sets
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public Linha getLinha() {
+		return linha;
+	}
+
+	public void setLinha(Linha linha) {
+		this.linha = linha;
+	}
+	
+	
+	
+	
+	
     
 }
